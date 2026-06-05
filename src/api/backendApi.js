e51993a7bde791460718ddafc221a60d95f14172
cauaosp <cauaosp20@gmail.com>
2026-06-05 14:43:50 -0300

    refactor: ajustado api com localhost fixo para dinâmico

--- src/api/backendApi.js
+++ src/api/backendApi.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/api/v1";
+const API_URL = "/api/v1";
 
 export async function buscarClimaCidade(cidade) {
   const response = await fetch(`${API_URL}/clima/${cidade}`);
